8327830f4d927ac0c3717c58ab6de164f92c1233
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-08-31 17:15:10 -0400

    Created new algo

---
+++ GreatestCommonDivisorOfStrings.js
@@ -0,0 +1,27 @@
+// Greatest Common Divisor of Strings
+
+/*
+For two strings 's' and 't', we say "t divides s" if and
+only if 's = t + ... + t' (ie., t is concatenated with 
+itself one or more times).
+
+Given two strings str1 and str2, return the largest string 'x'
+such that 'x' divides both str1 and str2.
+*/
+
+
+// Inputs
+const str1A = "ABCABC"
+const str2A = "ABC"
+
+const str1B = "ABABAB"
+const str2B = "ABAB"
+
+const str1C = "LEET"
+const str2C = "CODE"
+
+
+// Function
+const gcdOfStrings = (str1, str2) => {
+    
+}
