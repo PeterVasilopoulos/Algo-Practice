3db6e78f67ba4fe702db967ac12e444242897e35
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-09-02 02:30:28 -0400

    Created algo code

---
+++ ReverseWordsInAString.js
@@ -0,0 +1,39 @@
+// Reverse Words in a String
+
+/*
+Given an input string s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters.
+The words in s will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated
+by a single space.
+
+Note that s may contain leading or trailing spaces or multiple
+spaces between two words. The returned string should only have
+a single space separating the words. Do not include any extra
+spaces.
+*/
+
+
+// Inputs
+const s1 = "the sky is blue"
+// output = blue is sky the
+
+const s2 = "   hello world  "
+// output = world hello
+
+const s3 = "a good    example"
+// output = example good a
+
+
+// Function
+const reverseWords = (s) => {
+
+}
+
+
+// Testing
+console.log(reverseWords(s1))
+console.log(reverseWords(s2))
+console.log(reverseWords(s3))
