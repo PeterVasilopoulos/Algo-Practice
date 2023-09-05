641587b457659a7341f4fcb8b8a371206879248f
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-09-05 07:41:18 -0400

    Algo completed

--- ProductOfArrayExceptSelf.js
+++ ProductOfArrayExceptSelf.js
@@ -8,7 +8,7 @@ elements of nums except nums[i].
 The product of any prefix or suffix of nums is guaranteed
 to fit in a 32-bit integer.
 
-You must write an algorithm that runs in 0(n) time and
+You must write an algorithm that runs in O(n) time and
 without using the division operation.
 */
 
@@ -23,7 +23,35 @@ const nums2 = [-1,1,0,-3,3]
 
 // Function
 const productExceptSelf = (nums) => {
+    // create a left array
+    let leftArr = []
+    // create a left multiplication value
+    let leftMult = 1
 
+    // increment through nums
+    for(let i = 0; i < nums.length; i++) {
+        // set leftArr[i] = leftMult
+        leftArr[i] = leftMult
+        // multiply leftMult by the current nums value
+        leftMult *= nums[i]
+    }
+
+    // create a right array
+    let rightArr = []
+    // create a right multiplication value
+    let rightMult = 1
+
+    // decrement through nums
+    for(let i = nums.length - 1; i >= 0; i--) {
+        // set rightArr[i] = rightMult
+        rightArr[i] = rightMult
+        // multiply rightMult by the current nums value
+        rightMult *= nums[i]
+        // multiply current rightArr value by leftArr value
+        rightArr[i] *= leftArr[i]
+    }
+
+    return rightArr
 }
 
 
