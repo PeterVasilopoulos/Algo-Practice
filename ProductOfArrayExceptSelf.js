25ddb1474755bd33dc23f9bf44cd86666b7ab76c
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-09-05 07:02:59 -0400

    Created algo

---
+++ ProductOfArrayExceptSelf.js
@@ -0,0 +1,32 @@
+// Product of Array Except Self
+
+/*
+Given an integer array nums, return an array answer
+such that answer[i] is equal to the product of all the
+elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed
+to fit in a 32-bit integer.
+
+You must write an algorithm that runs in 0(n) time and
+without using the division operation.
+*/
+
+
+// Inputs
+const nums1 = [1,2,3,4]
+// output = [24,12,8,6]
+
+const nums2 = [-1,1,0,-3,3]
+// output = [0,0,9,0,0]
+
+
+// Function
+const productExceptSelf = (nums) => {
+
+}
+
+
+// Testing
+console.log(productExceptSelf(nums1))
+console.log(productExceptSelf(nums2))
