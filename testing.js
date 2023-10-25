e38bf3e0ea52d351c4d78a2991f2bd23873fb824
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-10-25 12:41:24 -0400

    Doing some testing

--- testing.js
+++ testing.js
@@ -1,10 +1,17 @@
-const test = "pog "
+// const test = "pog "
 
-for(let i = 0; i < test.length +1; i++) {
-    if(test[i]) {
-        console.log("yes")
-    } else {
-        console.log("no")
-    }
-}
+// for(let i = 0; i < test.length +1; i++) {
+//     if(test[i]) {
+//         console.log("yes")
+//     } else {
+//         console.log("no")
+//     }
+// }
 
+let arr = [1, 2, 3]
+
+console.log(arr)
+
+arr.unshift(0)
+
+console.log(arr)
