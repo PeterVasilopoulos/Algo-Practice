571dd913345ef60072ac0e5975e2bd2904dbaef6
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-10-25 14:23:34 -0400

    edited testing file

--- testing.js
+++ testing.js
@@ -8,14 +8,47 @@
 //     }
 // }
 
-let arr = [1, 2, 3]
+// let arr = [1, 2, 3]
 
-console.log(arr)
+// console.log(arr)
 
-arr.unshift(0)
+// arr.unshift(0)
 
-console.log(arr)
+// console.log(arr)
 
-let sum = 10
-sum -= 3
-console.log(sum)
+// let sum = 10
+// sum -= 3
+// console.log(sum)
+
+console.log(2**0)
+
+// variable to hold the sum, start at value of i
+let sum = i
+    
+while(i !== j) {
+    // check if j is < or > i
+    if(j < i) {
+        i--
+    } else {
+        i++
+    }
+    
+    // add value to sum
+    sum += i
+    console.log("I: " + i + " SUM: " + sum)
+}
+
+while(j !== k) {
+    // check if k is < or > j
+    if(k < j) {
+        j--
+    } else {
+        j++
+    }
+    
+    // add value to sum
+    sum += j
+    console.log("I: " + i + " SUM: " + sum)
+}
+
+return sum
