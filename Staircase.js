d4572ba07b1a777069d67ca0e780cb5ca2ee1adc
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2024-01-21 12:17:42 -0500

    Algo completed

--- Staircase.js
+++ Staircase.js
@@ -3,7 +3,24 @@ const stair2 = 7
 
 
 function staircase(n) {
-    
+    // for loop that runs until the n value is reached
+    for(let i = 1; i <= n; i++) {
+        // create a string to return
+        let str = ""
+
+        // for loop to add the spaces
+        for(let j = n; j > i; j--) {
+            str += " "
+        }
+
+        // for loop to add the #s
+        for(let k = 1; k <= i; k++) {
+            str += "#"
+        }
+
+        // log str
+        console.log(str)
+    }
 }
 
 
